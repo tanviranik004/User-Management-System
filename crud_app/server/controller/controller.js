637608ae7f7e202a8687375ce938884f7129befe
tanviranik004 <tanvir.anik.009@gmail.com>
2022-09-28 15:59:15 +0600

    post-get-update-delete

--- crud_app/server/controller/controller.js
+++ crud_app/server/controller/controller.js
@@ -1,4 +1,5 @@
 
+const { findByIdAndUpdate } = require('../model/model');
 var Userdb = require('../model/model');
 
 
@@ -38,17 +39,54 @@ exports.create = (req, res) => {
 //retrieve and return users  / retrieve and return a single user
 
 exports.find = (req, res) => {
-    
+    Userdb.find()
+    .then(user=>{
+      res.send(user)
+    })
+    .catch(err=>{
+      res.status(500).send({message:err.message || "Error Occurred while Retrieving user information"})
+    })
 }
 
 //Update a new identified user by user id
 
 exports.update = (req, res) => {
-
+  if(!req.body){
+    return res
+      .status(400)
+      .send({message:"Data to update can not be empty"})
+  }
+  const id=req.params.id;
+  Userdb.findByIdAndUpdate(id,req.body,{useFindAndModify:false})
+    .then(data=>{
+      if(!data){
+        res.status(404).send({message:`Cannot Update user with ${id}.Maybe user not found`})
+      }else{
+        res.send(data)
+      }
+    })
+    .catch(err=>{
+      res.status(500).send({message:"Error Update user information"})
+    })
 }
 
 //Delete a User with specified user in the request  
 
 exports.delete = (req, res) => {
-
+  const id =req.params.id;
+  Userdb.findByIdAndDelete(id)
+    .then(data=>{
+      if(!data){
+        res.status(404).send({message: `Cannot Delete with id ${id}. Maybe id is wrong`})
+      }else{
+        res.send({
+          message:"User was deleted successfully!"
+        })
+      }
+    })
+    .catch(err=>{
+      res.status(500).send({
+        message: "Could not delete User with id=" +id
+      });
+    });
 }
